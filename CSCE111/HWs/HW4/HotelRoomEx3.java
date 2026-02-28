0675f3aa8848c82b4e25423ec8fc9068f465873f
Manas Param <m4nasp06@gmail.com>
2026-02-28 14:56:56 -0600

    hw4 finished

--- CSCE111/HWs/HW4/HotelRoomEx3.java
+++ CSCE111/HWs/HW4/HotelRoomEx3.java
@@ -6,24 +6,30 @@ import java.util.Scanner;
 
 public class HotelRoomEx3 {
 
+    public static Scanner scnr = new Scanner(System.in);
+    public static int startingFloor = 0;
+    public static int floors = 0;
+    public static int topFloor = 0;
+    public static int roomsPerFloor = 0;
+
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
         getInfo(scnr);
-        
+        rosterDisplay();
+
         scnr.close();
     }
 
     public static void getInfo(Scanner scnr) {
         System.out.print("Enter the starting floor: ");
-        int startingFloor = scnr.nextInt();
+        startingFloor = scnr.nextInt();
         if (startingFloor < 3 || startingFloor > 6) {
             System.out.println("Starting floor must be between 3 and 6.");
             return;
         }
         System.out.print("Enter the total number of floors:");
-        int floors = scnr.nextInt();
+        floors = scnr.nextInt();
 
-        int topFloor = startingFloor + floors;
+        topFloor = startingFloor + floors;
         if (topFloor < 13) {
             floors -= 1;
         }
@@ -34,12 +40,36 @@ public class HotelRoomEx3 {
         }
 
         System.out.print("Enter the number of rooms per floor: ");
-        int roomsPerFloor = scnr.nextInt();
+        roomsPerFloor = scnr.nextInt();
         if (roomsPerFloor < 1 || roomsPerFloor > 999) {
             System.out.println("Rooms per floor should be between 1 and 999");
             return;
         }
     }
-    
-    
+
+    public static void rosterDisplay() {
+        System.out.println("Hotel Room Cleaning Roster");
+        System.out.printf("%-20s", "Room Number");
+        System.out.printf("%-10s%n", "Cleaned");
+
+        for (int i = startingFloor; i <= topFloor; i++) {
+            if (i == 13) {
+                continue;
+            }
+            for (int j = 1; j <= roomsPerFloor; j++) {
+                int room;
+                if (roomsPerFloor < 100) {
+                    room = i * 100 + j;
+                } else {
+                    room = i * 1000 + j;
+                }
+                if (i == topFloor && j == roomsPerFloor) {
+                    System.out.printf("%-20d%-10s", room, "__________");
+                } else {
+                    System.out.printf("%-20d%-10s%n", room, "__________");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
